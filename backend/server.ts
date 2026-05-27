0d3ddda42e0ed5ac6cbc57af38be5d9290bb1582
stalstierna <andrea.stalstierna@chasacademy.se>
2026-05-27 11:52:19 +0200

    install ts

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-// import connectDB from "./config/db.js";
+import connectDB from "./config/db.js";
 dotenv.config();
 
 const app = express();
 
-// connectDB();
+connectDB();
 
 app.use(cors());
 app.use(express.json());
